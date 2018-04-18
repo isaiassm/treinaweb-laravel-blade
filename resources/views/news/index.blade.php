0eaf8747eaba67575b2a460010d4439db9059b16
Treinaweb <treinaweb@Treinaweb.local>
2018-04-18 17:23:59 -0300

    07 - código PHP e comentários

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -24,3 +24,15 @@
   {{ age }}
   {{ email }}
 @endverbatim
+
+@php
+  $myVar = 10;
+
+  $myVar = $myVar + 10;
+
+  echo $myVar;
+@endphp
+
+{{-- ficar somente no blade (php) --}}
+
+<!-- chegar no front-end -->
