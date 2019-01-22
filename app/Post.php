ea2e17a03831ac1ff11f33e1a04daf2e9c85db5c
isaiassm <isaias1113@outlook.com>
2019-01-22 17:23:45 -0200

    [UPDATE] Mapeando os dados do banco na model ultilizamos o protected table para passar a tabela e o primaryKey para o id e as constantes created_At e update_at para passar as datas de criacao

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    const CREATED_AT = 'created_at';
+    const UPDATE_AT = 'updated_at';
+
+    protected $table = 'posts';
+
+    protected $primaryKey = 'id';
+
 }
